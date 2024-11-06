f9a0e9d2fdfeb5963d5746544bc7ebc8abddf94c
bruno gatti <brunogatti@brunos-Laptop.local>
2024-11-06 13:50:14 +0100

    Quartz sync: Nov 6, 2024, 1:50 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,7 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [ProfilePicture()],
+  header: [],
   afterBody: [],
   footer: Component.Footer({
     links: {
